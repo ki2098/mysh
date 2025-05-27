be8f2ceaa1e36915143fb537f1a9b4bcfcbd1001
ki2098 <58096339+ki2098@users.noreply.github.com>
2025-05-27 19:14:23 +0900

    Update fish_prompt.fish

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -5,17 +5,33 @@ function fish_prompt --description 'Write out the prompt'
         set -q fish_color_status
         or set -g fish_color_status red
 
+        # Emphasize the main part of the prompt
+        set -U fish_color_host green --bold
+        set -U fish_color_user green --bold
+        set -g fish_color_cwd --bold
+
         # Since we display the prompt on a new line allow the directory names to be longer.
         set -q fish_prompt_pwd_dir_length
         or set -lx fish_prompt_pwd_dir_length 0
 
+        # Set git prompt styles
         set -q __fish_git_prompt_showdirtystate
         or set -g __fish_git_prompt_showdirtystate 1
+        set -q __fish_git_prompt_color_untrackedfiles
+        or set -g __fish_git_prompt_color_untrackedfiles red
+        set -q __fish_git_prompt_char_untrackedfiles
+        or set -g __fish_git_prompt_char_untrackedfiles '?'
         set -q __fish_git_prompt_showuntrackedfiles
         or set -g __fish_git_prompt_showuntrackedfiles 1
+        set -q __fish_git_prompt_color_branch_dirty
+        or set -g __fish_git_prompt_color_branch_dirty red
+        set -q __fish_git_prompt_char_dirtystate
+        or set -g __fish_git_prompt_char_dirtystate '*'
+        set -q __fish_git_prompt_color
+        or set -g __fish_git_prompt_color white
     
         # Color the prompt differently when we're root
-        set -l color_cwd $fish_color_cwd
+        set -l color_cwd $fish_color_cwd --bold
         set -l suffix '>'
         if functions -q fish_is_root_user; and fish_is_root_user
                 if set -q fish_color_cwd_root
